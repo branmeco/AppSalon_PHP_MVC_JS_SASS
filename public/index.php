8b06b790aea761117c227a1a1dd2311811cea798
branmeco <branmeco17@gmail.com>
2023-03-07 18:07:29 -0500

    Creando el Routing y Controlador para Autenticación

--- public/index.php
+++ public/index.php
@@ -2,11 +2,28 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 
 $router = new Router();
 
+//Iniciar Sesión
 
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+
+//Cerrar sesión
+$router->get('/logout', [LoginController::class, 'logout']);
+
+//Recuperar password
+$router->get('/olvide', [LoginController::class, 'olvide']);
+$router->post('/olvide', [LoginController::class, 'olvide']);
+$router->get('/recuperar', [LoginController::class, 'recuperar']);
+$router->post('/recuperar', [LoginController::class, 'recuperar']);
+
+//Crear cuenta
+$router->get('/crear-cuenta', [LoginController::class, 'crear']);
+$router->post('/crear-cuenta', [LoginController::class, 'crear']);
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
