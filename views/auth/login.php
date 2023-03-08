ab073c6ebd37c3557c4ec4fd3280613fe527749c
branmeco <branmeco17@gmail.com>
2023-03-07 19:13:17 -0500

    Crear el formulario de autenticación

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,3 +1,19 @@
 <h1 class="nombre-pagina">Login</h1>
 <p class="descripcion-pagina">Inicia sesión cons tus datos</p>
 
+<form action="/" class="formulario" method="POST">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="Tu Email" name="email"/>
+    </div>
+    <div class="campo">
+        <label for="passwor">Password</label>
+        <input type="password" id="password" placeholder="Tu Password" name="password"/>
+    </div>
+    <input type="submit" class="boton" value="Iniciar Sessión"/>
+</form>
+
+<div class="acciones">
+    <a href="/crear-cuenta">¿Aún no tienes una cuenta? Crear una</a>
+    <a href="/olvide">¿Olvidaste tu password?</a>
+</div>
