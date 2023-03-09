58a4056c868247abeadaaaf5a383079300360807
branmeco <branmeco17@gmail.com>
2023-03-09 09:11:26 -0500

    Creando un template de alertas

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -28,12 +28,17 @@ class LoginController
     public static function crear(Router $router)
     {
         $usuario = new Usuario($_POST);
+
+        //Alertas vacias
+        $alertas = [];
+
         if($_SERVER['REQUEST_METHOD']==='POST'){
             $usuario->sincronizar($_POST);
             $alertas = $usuario->validarNuevaCuenta();
         }
         $router->render('auth/crear-cuenta', [
-            'usuario' => $usuario
+            'usuario' => $usuario,
+            'alertas' => $alertas
         ]);
     }
 }
--- views/auth/crear-cuenta.php
+++ views/auth/crear-cuenta.php
@@ -1,6 +1,10 @@
 <h1 class="nombre-pagina">Crear cuenta</h1>
 <p class="descripcion-pagina">Llena el siguiente el formulario para crear una cuenta</p>
 
+<?php
+    include_once __DIR__ . "/../templates/alertas.php";
+?>
+
 <form class="formulario" method="POST" action="/crear-cuenta">
     <div class="campo">
         <label for="nombre">nombre</label>
@@ -8,15 +12,15 @@
     </div>
     <div class="campo">
         <label for="apellido">apellido</label>
-        <input type="text" id="apellido" name="apellido" placeholder="Tu Apellido" value="<?php echo s($usuario->apellido);?>/>
+        <input type="text" id="apellido" name="apellido" placeholder="Tu Apellido" value="<?php echo s($usuario->apellido);?>"/>
     </div>
     <div class="campo">
         <label for="telefono">telefono</label>
-        <input type="tel" id="telefono" name="telefono" placeholder="Tu Teléfono" value="<?php echo s($usuario->telefono);?>/>
+        <input type="tel" id="telefono" name="telefono" placeholder="Tu Teléfono" value="<?php echo s($usuario->telefono);?>"/>
     </div>
     <div class="campo">
         <label for="email">E-mail</label>
-        <input type="email" id="email" name="email" placeholder="Tu E-mail" value="<?php echo s($usuario->email);?>/>
+        <input type="email" id="email" name="email" placeholder="Tu E-mail" value="<?php echo s($usuario->email);?>"/>
     </div>
     <div class="campo">
         <label for="password">Password</label>
