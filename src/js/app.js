bae1b588ced7c9dc1a77677d280ad6253554ffb0
branmeco <branmeco17@gmail.com>
2023-03-16 22:29:29 -0500

    Aplicando CSS al resumen

--- src/js/app.js
+++ src/js/app.js
@@ -236,19 +236,16 @@ const mostrarResumen = () => {
     }
 
     // Formatear el div de resumen
-    const {nombre, fecha, hora, servicios} = cita;
+    const { nombre, fecha, hora, servicios } = cita;
 
-    const nombreCliente = document.createElement('P');
-    nombreCliente.innerHTML = `<span>Nombre:</span> ${nombre}`;
-    
-    const fechaCita = document.createElement('P');
-    fechaCita.innerHTML = `<span>Fecha:</span> ${fecha}`;
+    //Heading para servicios en resumen
+    const headingServicios = document.createElement('H3');
+    headingServicios.textContent = 'Resumen de Servicios';
+    resumen.appendChild(headingServicios);
 
-    const horaCita = document.createElement('P');
-    horaCita.innerHTML = `<span>Hora:</span> ${hora}`;
-
+    //Iterando y mostrando los servicios
     servicios.forEach(servicio => {
-        const {id, precio, nombre} = servicio;
+        const { id, precio, nombre } = servicio;
 
         const contenedorServicio = document.createElement('DIV');
         contenedorServicio.classList.add('contenedor-servicio');
@@ -264,6 +261,21 @@ const mostrarResumen = () => {
 
         resumen.appendChild(contenedorServicio);
     });
+
+    //Heading para cita en resumen
+    const headingCita = document.createElement('H3');
+    headingCita.textContent = 'Resumen de Cita';
+    resumen.appendChild(headingCita);
+
+
+    const nombreCliente = document.createElement('P');
+    nombreCliente.innerHTML = `<span>Nombre:</span> ${nombre}`;
+
+    const fechaCita = document.createElement('P');
+    fechaCita.innerHTML = `<span>Fecha:</span> ${fecha}`;
+
+    const horaCita = document.createElement('P');
+    horaCita.innerHTML = `<span>Hora:</span> ${hora} Horas`;
 
     resumen.appendChild(nombreCliente);
     resumen.appendChild(fechaCita);
