8f78fc4f7c13fe0672ce93383df2cfdca936f7e6
branmeco <branmeco17@gmail.com>
2023-03-20 14:48:04 -0500

    Guardando los Servicios de la Cita

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -1,22 +1,42 @@
 <?php
+
 namespace Controllers;
 
 use Model\Servicio;
 use Model\Cita;
+use Model\CitaServicio;
 
-class APIController {
-    public static function index(){
+class APIController
+{
+    public static function index()
+    {
         $servicios = Servicio::all();
 
         echo json_encode($servicios);
     }
 
-    public static function guardar(){
+    public static function guardar()
+    {
 
+        // Almacena la Cita y devuelve el ID
         $cita = new Cita($_POST);
+        $resultado = $cita->guardar();
 
-        $resultado = $cita -> guardar();
+        $id = $resultado['id'];
 
-        echo json_encode($resultado);
+        // Almacena la Cita y el Servicio
+
+        // Almacena los Servicios con el ID de la Cita
+        $idServicios = explode(",", $_POST['servicios']);
+        foreach ($idServicios as $idServicio) {
+            $args = [
+                'citaId' => $id,
+                'servicioId' => $idServicio
+            ];
+            $citaServicio = new CitaServicio($args);
+            $citaServicio->guardar();
+        }
+
+        echo json_encode(['resultado' => $resultado]);
     }
 }
