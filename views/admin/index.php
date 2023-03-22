1026d80226ae2c5aa7eb8c2585b942a096c3e479
branmeco <branmeco17@gmail.com>
2023-03-22 09:59:07 -0500

    Mostrar las Citas en pantalla

--- views/admin/index.php
+++ views/admin/index.php
@@ -9,10 +9,31 @@ include_once __DIR__ . '/../templates/barra.php';
     <form class="formulario">
         <div class="campo">
             <label for="fecha">Fecha</label>
-            <input type="date" id="fecha" name="fecha"/>
+            <input type="date" id="fecha" name="fecha" />
         </div>
     </form>
 </div>
-<div class="citas-admin">
 
+<div class="citas-admin">
+    <ul class="citas">
+        <?php
+            $idCita = 0;
+            foreach ($citas as $cita) {
+                if ($idCita !== $cita->id) {
+        ?>
+                <li>
+                    <p>ID: <span><?php echo $cita->id; ?></span></p>
+                    <p>Hora: <span><?php echo $cita->hora; ?></span></p>
+                    <p>Cliente: <span><?php echo $cita->cliente; ?></span></p>
+                    <p>E-mail: <span><?php echo $cita->email; ?></span></p>
+                    <p>Teléfono: <span><?php echo $cita->telefono; ?></span></p>
+
+                    <h3>Servicios</h3>
+                <?php 
+                    $idCita = $cita->id;
+                } //fin de if ?>
+                <p class="servicio"><?php echo $cita->servicio . " " . $cita->precio; ?></p>
+                </li>
+            <?php } //fin de foreach ?>
+    </ul>
 </div>
