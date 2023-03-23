a7f7292cd4bee975c64daad2304f403122d679ee
branmeco <branmeco17@gmail.com>
2023-03-23 18:52:42 -0500

    validación a los servicios

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers;
 
+use Model\Servicio;
 use MVC\Router;
 
 class ServicioController
@@ -11,26 +12,36 @@ class ServicioController
 
         session_start();
 
-        $router->render('services/index', [
+        $router->render('servicios/index', [
             'nombre' => $_SESSION['nombre']
         ]);
     }
     public static function crear(Router $router)
     {
-
         session_start();
 
+        $servicio = new Servicio;
+        $alertas = [];
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $servicio->sincronizar($_POST);
+            $alertas = $servicio->validar();
+            if(empty($alertas)){
+                $servicio->guardar();
+                header('Location: /servicios');
+            }
         }
-        $router->render('services/crear', [
-            'nombre' => $_SESSION['nombre']
+        $router->render('servicios/crear', [
+            'nombre' => $_SESSION['nombre'],
+            'servicio' => $servicio,
+            'alertas' => $alertas
         ]);
     }
     public static function actualizar(Router $router)
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
-        $router -> render('services/actualizar', [
+        $router->render('servicios/actualizar', [
             'nombre' => $_SESSION['nombre']
         ]);
     }
