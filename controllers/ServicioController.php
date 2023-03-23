d44eccc4dcbee55b3dff5ccc9b142a2484e7e507
branmeco <branmeco17@gmail.com>
2023-03-23 18:11:19 -0500

    Creando las vista para los servicios

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -4,26 +4,39 @@ namespace Controllers;
 
 use MVC\Router;
 
-class ServicioController {
-    public static function index(Router $router){
-        $router -> render('services/index', [
+class ServicioController
+{
+    public static function index(Router $router)
+    {
 
+        session_start();
+
+        $router->render('services/index', [
+            'nombre' => $_SESSION['nombre']
         ]);
     }
-    public static function crear(Router $router){
-        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    public static function crear(Router $router)
+    {
 
-        }
-    }
-    public static function actualizar(Router $router){
-        if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    
-        }
-    }
-    public static function eliminar(Router $router){
-        if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    
-        }
-    }
+        session_start();
 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        }
+        $router->render('services/crear', [
+            'nombre' => $_SESSION['nombre']
+        ]);
+    }
+    public static function actualizar(Router $router)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        }
+        $router -> render('services/actualizar', [
+            'nombre' => $_SESSION['nombre']
+        ]);
+    }
+    public static function eliminar(Router $router)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        }
+    }
 }
